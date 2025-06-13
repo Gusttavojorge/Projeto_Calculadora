cada367b12c8ecea2c2fd3e7db5f8b2b0917d2bb
Gustavo Jorge <ceogusttavojorgge@gmail.com>
2025-06-13 16:43:31 -0300

    Implementando códigos automatizados de testes em CalculadoraTest.java

---
+++ src/main/java/com/exemplo/calculadora/Calculadora.java
@@ -0,0 +1,26 @@
+package com.exemplo.calculadora;
+
+// Classe Calculadora
+// Contém métodos básicos para operações aritméticas.
+ 
+public class Calculadora {
+
+    public double somar(double a, double b) {
+        return a + b;
+    }
+
+    public double subtrair(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplicar(double a, double b) {
+        return a * b;
+    }
+
+    public double dividir(double a, double b) {
+        if (b == 0) {
+            throw new ArithmeticException("Divisão por zero não é permitida!");
+        }
+        return a / b;
+    }
+}
