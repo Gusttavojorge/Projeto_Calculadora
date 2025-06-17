07e0daae5ffe61654b0e71d4c6a2f14f29dbe9ec
Gustavo Jorge <ceogusttavojorgge@gmail.com>
2025-06-17 15:58:59 -0300

    Refatorada lógica de divisão para evitar exceção ao dividir por zero
    Adicionado laço com tratamento de erro na operação de divisão.
    Agora o programa solicita novo valor até entrada válida, evitando falha na execução.

--- src/main/java/br/gustavojorge/calculadora/program/Program.java
+++ src/main/java/br/gustavojorge/calculadora/program/Program.java
@@ -10,59 +10,67 @@ public class Program {
     public static void main(String[] args) {
 
         Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
 
-        Calculadora calc = new Calculadora();
+        try (Scanner sc = new Scanner(System.in)) {
+            Calculadora calc = new Calculadora();
 
-        System.out.println("Iniciando Calculadora!");
-        System.out.println("");
+            System.out.println("Iniciando Calculadora!\n");
 
-        char resp;
-        do {
-            System.out.println("Operações disponiveis:");
-            System.out.println("1 - Soma");
-            System.out.println("2 - Divisão");
-            System.out.println("3 - Subtração");
-            System.out.println("4 - Multiplicação");
-            System.out.println("");
+            char resp = 0;
+            do {
+                System.out.println("Operações disponíveis:");
+                System.out.println("1 - Soma");
+                System.out.println("2 - Divisão");
+                System.out.println("3 - Subtração");
+                System.out.println("4 - Multiplicação\n");
 
-            System.out.println("Digite a operação desejada (1-4):");
-            System.out.print("nº ");
-            int num = sc.nextInt();
-            System.out.println("");
+                System.out.print("Digite a operação desejada (1-4): \n");
+                int num = sc.nextInt();
+                System.out.println();
 
-            System.out.println("Digite o primeiro número: ");
-            double num1 = sc.nextInt();
+                System.out.print("Digite o primeiro número: \nnº ");
+                double num1 = sc.nextDouble();
 
-            System.out.println("Digite o segundo número: ");
-            double num2 = sc.nextInt();
-            System.out.println("");
+                double num2 = 0;
 
-            switch (num) {
-                case 1:
-                    calc.setTotal(calc.soma(num1, num2));
-                    break;
-                case 2:
-                    calc.setTotal(calc.divisao(num1, num2));
-                    break;
-                case 3:
-                    calc.setTotal(calc.subtracao(num1, num2));
-                    break;
-                case 4:
-                    calc.setTotal(calc.multiplicacao(num1, num2));
-                    break;
-            }
+                if (num == 2) {
+                    boolean valido = false;
+                    while (!valido) {
+                        System.out.print("Digite o segundo número: \nnº ");
+                        num2 = sc.nextDouble();
+                        try {
+                            calc.setTotal(calc.divisao(num1, num2));
+                            valido = true;
+                        } catch (ArithmeticException e) {
+                            System.out.println("Erro: " + e.getMessage());
+                            System.out.println("Por favor, insira um número diferente de zero.\n");
+                        }
+                    }
+                } else {
+                    System.out.print("Digite o segundo número: \nnº ");
+                    num2 = sc.nextDouble();
 
-            System.out.println(calc);
-            System.out.println("");
+                    switch (num) {
+                        case 1:
+                            calc.setTotal(calc.soma(num1, num2));
+                            break;
+                        case 3:
+                            calc.setTotal(calc.subtracao(num1, num2));
+                            break;
+                        case 4:
+                            calc.setTotal(calc.multiplicacao(num1, num2));
+                            break;
+                        default:
+                            System.out.println("Operação inválida!");
+                            continue; // volta ao início do loop
+                    }
+                }
 
-            System.out.println("Deseja realizar outra operação: (s/n)");
-            resp = sc.next().charAt(0);
+                System.out.println("\n" + calc);
+                System.out.print("\nDeseja realizar outra operação? (s/n): \n");
+                resp = sc.next().charAt(0);
 
-        } while (resp != 'n' && resp != 'N');
-
-        sc.close();
+            } while (resp != 'n' && resp != 'N');
+        }
     }
 }
-
-// Comentário de teste feito na branch feature/calculadora
